aa26b6c98a2882975a710812c153026b0137701a
sunghoan-we <javarajava@hotmail.com>
2023-09-27 01:54:28 -0700

    New Service Registry
    Create New Service Registry

---
+++ src/test/java/springboot/demo/serviceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.demo.serviceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
